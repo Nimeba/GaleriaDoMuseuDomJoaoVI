5be2cf450646ea667767d0cde9215ed8ae96af38
Nimeba <azambuja.c@gmail.com>
2015-01-13 12:30:33 -0200

    activity e xml de contatos atualizada. falta as imagens dos botoes e rodar para ver se esta funcionando.

--- src/com/example/galeriadomuseudomjoaovi/Contatos.java
+++ src/com/example/galeriadomuseudomjoaovi/Contatos.java
@@ -2,14 +2,32 @@ package com.example.galeriadomuseudomjoaovi;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class Contatos extends Activity {
+public class Contatos extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contatos);
+		
+		ImageButton voltar = (ImageButton) findViewById(R.id.btnVoltar);
+		voltar.setOnClickListener(this);
+		
+		ImageButton agradecimentos = (ImageButton) findViewById(R.id.btnAgradecimentos);
+		agradecimentos.setOnClickListener(this);
+		
+		ImageButton contatos = (ImageButton) findViewById(R.id.btnContatos);
+		contatos.setOnClickListener(this);
+		
+		ImageButton equipe = (ImageButton) findViewById(R.id.btnEquipe);
+		equipe.setOnClickListener(this);
+		
 	}
 
 	@Override
@@ -19,4 +37,40 @@ public class Contatos extends Activity {
 		return true;
 	}
 
+	@Override
+	public void onClick(View v) {
+		
+		ImageView imgContatos = (ImageView) findViewById(R.id.imgContatos);
+		ImageView imgAgradecimentos = (ImageView) findViewById(R.id.imgAgradecimentos);
+		ImageView imgEquipe = (ImageView) findViewById(R.id.imgEquipe);
+		
+		
+		switch (v.getId()) {
+		
+			case R.id.btnAgradecimentos:
+				imgAgradecimentos.setVisibility(imgAgradecimentos.VISIBLE);
+				imgContatos.setVisibility(imgContatos.INVISIBLE);
+				imgEquipe.setVisibility(imgEquipe.INVISIBLE);
+			break;
+		
+			case R.id.btnContatos:
+				imgContatos.setVisibility(imgContatos.VISIBLE);
+				imgAgradecimentos.setVisibility(imgAgradecimentos.INVISIBLE);
+				imgEquipe.setVisibility(imgEquipe.INVISIBLE);
+			break;
+		
+			case R.id.btnEquipe:
+				imgEquipe.setVisibility(imgEquipe.VISIBLE);
+				imgAgradecimentos.setVisibility(imgAgradecimentos.INVISIBLE);
+				imgContatos.setVisibility(imgContatos.INVISIBLE);
+			break;
+		
+			case R.id.btnVoltar:
+				startActivity(new Intent(Contatos.this, MainActivity.class));
+			break;
+		
+			default:
+				break;
+		}	
+	}
 }
