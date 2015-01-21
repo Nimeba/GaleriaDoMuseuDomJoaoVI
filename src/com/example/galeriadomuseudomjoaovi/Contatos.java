e2698973ed5f251e872d17ffeaf468a915df9ca7
Nimeba <azambuja.c@gmail.com>
2015-01-21 16:03:37 -0200

    Classe contatos atualizada. Faltam apenas pequenas alteraçöes nas imagens dos botoes.

--- src/com/example/galeriadomuseudomjoaovi/Contatos.java
+++ src/com/example/galeriadomuseudomjoaovi/Contatos.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 
 public class Contatos extends Activity implements OnClickListener {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
