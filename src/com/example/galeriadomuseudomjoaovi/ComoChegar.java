f961477801b496ab71fc3026e78c12aacd048985
Nimeba <azambuja.c@gmail.com>
2015-01-22 17:18:56 -0200

    Como chegar já com o mapa e a localizaçao da reitoria

--- src/com/example/galeriadomuseudomjoaovi/ComoChegar.java
+++ src/com/example/galeriadomuseudomjoaovi/ComoChegar.java
@@ -1,15 +1,21 @@
 package com.example.galeriadomuseudomjoaovi;
 
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class ComoChegar extends Activity {
+public class ComoChegar extends Activity implements OnMapReadyCallback{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_como_chegar);
+		MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+		
 	}
 
 	@Override
@@ -18,5 +24,18 @@ public class ComoChegar extends Activity {
 		getMenuInflater().inflate(R.menu.como_chegar, menu);
 		return true;
 	}
+	
+	@Override
+    public void onMapReady(GoogleMap map) {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(-22.863259, -43.222995), 16));
+
+        // You can customize the marker image using images bundled with
+        // your app, or dynamically generated bitmaps.
+        map.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher))
+                .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+                .position(new LatLng(-22.863259, -43.222995)));
+    }
 
 }
---
+++ src/util/screenslide/MapFragment.java
@@ -0,0 +1,8 @@
+package util.screenslide;
+
+import android.app.Fragment;
+
+public class MapFragment extends Fragment{
+	
+	
+}
