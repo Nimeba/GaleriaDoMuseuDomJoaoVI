0922366dd5b0eedf5764bfd83ee8e89f5e4a3019
deoval <deovaljr@gmail.com>
2015-03-15 20:37:58 -0300

    Menu lateral na galeria

---
+++ src/util/ListViewFragment.java
@@ -0,0 +1,11 @@
+package util;
+
+import android.support.v4.app.Fragment;
+
+
+public class ListViewFragment extends Fragment {
+
+	public ListViewFragment() {
+		// TODO Auto-generated constructor stub
+	}
+}
